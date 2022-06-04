e5551540fc676ce991e647b16cf0408cb9c1564a
Chris Langford <chris@hellocld.com>
2022-06-04 19:33:07 -0400

    Signal cleanup

--- Scenes/Hallways/TestHallway/Door.gd
+++ Scenes/Hallways/TestHallway/Door.gd
@@ -3,10 +3,6 @@ extends Spatial
 signal door_open()
 
 
-func _ready() -> void:
-	EventBus.connect("hall_completed", self, "_on_hall_completed")
-
-
 func _on_door_is_opened() -> void:
 	emit_signal("door_open")
 
