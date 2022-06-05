bdf21c5b462bf2a881930a4c53a7cc0f785ec7ff
Chris Langford <chris@hellocld.com>
2022-06-05 13:14:07 -0400

    Explosions

--- Scenes/Creatures/Enemies/HallCompleteSwitch/HallCompleteSwitch.gd
+++ Scenes/Creatures/Enemies/HallCompleteSwitch/HallCompleteSwitch.gd
@@ -9,4 +9,5 @@ export(NodePath) var Hallway_Path : NodePath
 func _on_Health_killed() -> void:
 	emit_signal("switch_destroyed")
 	EventBus.emit_signal("hall_completed")
+	._on_Health_killed()
 	call_deferred("queue_free")
--- Scenes/Effects/SimpleExplosion/SimpleExplosion.gd
+++ Scenes/Effects/SimpleExplosion/SimpleExplosion.gd
@@ -1,10 +1,16 @@
 extends Particles
 
+var audio_playing := true
+
 
 func _ready() -> void:
 	emitting = true
 
 
 func _physics_process(_delta) -> void:
-	if !emitting:
+	if !emitting && !audio_playing:
 		queue_free()
+
+
+func _on_AudioStreamPlayer3D_finished():
+	audio_playing = false
